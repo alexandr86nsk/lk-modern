896f2e918a16698b4b0a52e3aac38f9b244e26e8
a.mihajlov <a.mihajlov@otlnal.ru>
2020-07-28 18:47:46 +0700

    исправил авторизацию

--- src/api/qs.js
+++ src/api/qs.js
@@ -1,5 +1,5 @@
 const qs = SERVER_URL === 'true'
   ? `http://${window.location.hostname}:20000/api/`
-  : 'http://nsk-1cprog-01:801/';
+  : 'http://185.91.53.212:801/';
 // const qs = 'http://nsk-dev-04:20000/api/';
 export default qs;
