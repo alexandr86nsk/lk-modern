ed418be448d54f37889aa774076f0d3fc6f758ef
a.mihajlov <MAXUSreal123>
2020-08-18 16:18:48 +0700

    тест

--- src/api/qs.js
+++ src/api/qs.js
@@ -1,5 +1,6 @@
-const qs = SERVER_URL === 'true'
+/*const qs = SERVER_URL === 'true'
   ? `http://${window.location.hostname}:20000/api/`
-  : 'http://10.168.1.203:8081/';
+  : 'http://10.168.1.203:8081/';*/
 // const qs = 'http://nsk-1cprog-01:801/';
+const qs = 'http://10.168.1.203:8081/';
 export default qs;
