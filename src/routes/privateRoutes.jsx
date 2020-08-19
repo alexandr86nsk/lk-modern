c53feac42bda001e999e622cfaed365983f493e3
a.mihajlov <MAXUSreal123>
2020-08-19 13:33:32 +0700

    тест

--- src/routes/privateRoutes.jsx
+++ src/routes/privateRoutes.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const transition = { duration: 0.5, ease: [0.43, 0.13, 0.23, 0.96] };
+
 const PrivateRoute = ({ isLoggedIn, component, ...props }) => (
   <Route
     {...props}
@@ -9,8 +11,9 @@ const PrivateRoute = ({ isLoggedIn, component, ...props }) => (
       isLoggedIn ? (
         <motion.div
           initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
+          animate={{ opacity: 1, transition }}
+          exit={{ opacity: 0, transition: { duration: 1.5, ...transition } }}
+          transition={transition}
         >
           {component}
         </motion.div>
