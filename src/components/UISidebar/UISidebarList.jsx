9d465e736c11cb1f4081ec7bed82b539f68ae4b8
a.mihajlov <a.mihajlov@otlnal.ru>
2020-09-25 21:30:25 +0700

    2

--- src/components/UISidebar/UISidebarList.jsx
+++ src/components/UISidebar/UISidebarList.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReportsIcon from '../../static/images/insert_chart_outlined-24px.svg';
 import ZoneIcon from '../../static/images/language-24px.svg';
 import SettingsIcon from '../../static/images/settings-24px.svg';
-import CalendarIcon from '../../static/images/date_range-24px.svg';
-import TestIcon from '../../static/images/business_center-24px.svg';
+import MonitoringIcon from '../../static/images/tv-24px.svg';
 
 const UISidebarList = [
   {
@@ -27,9 +26,16 @@ const UISidebarList = [
         id: 2,
         title: 'Мониторинг',
         link: '/reports',
+        icon: <MonitoringIcon />,
+      },
+      {
+        id: 2,
+        title: 'Отчеты',
+        link: '/reports',
         icon: <ReportsIcon />,
       },
     ],
   },
 ];
+
 export default UISidebarList;
