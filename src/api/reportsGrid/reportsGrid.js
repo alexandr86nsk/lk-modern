5ab93f9f134aa9cc7497b6da6617d2c4054c9feb
Real <real_86@ngs.ru>
2020-10-06 17:06:49 +0700

    2

--- src/api/reportsGrid/reportsGrid.js
+++ src/api/reportsGrid/reportsGrid.js
@@ -1,15 +1,25 @@
-import axios from 'axios';
-import qs from '../qs';
 import requestParser from '../requestParser';
 
-export const getJobDetailReport = (data) => axios.get(`${qs}Reports/JobDetailReport?briefcaseId=${data.selectedBriefcase || ''}`);
+export const getJobDetailReport = (data) => requestParser({
+  method: 'get',
+  url: 'Reports/JobDetailReport',
+  params: data,
+});
 
 export const getJobStatusReport = (data) => requestParser({
   method: 'get',
   url: 'Reports/JobStatusReport',
   params: data,
-})
+});
 
-export const getJobHistoryReport = (data) => axios.get(`${qs}Reports/JobHistoryReport?briefcaseId=${data.selectedBriefcase || ''}`);
+export const getJobHistoryReport = (data) => requestParser({
+  method: 'get',
+  url: 'Reports/JobHistoryReport',
+  params: data,
+});
 
-export const getJobCallHandlingReport = (data) => axios.get(`${qs}Reports/JobCallHandlingReport?briefcaseId=${data.selectedBriefcase || ''}`);
+export const getJobCallHandlingReport = (data) => requestParser({
+  method: 'get',
+  url: 'Reports/JobCallHandlingReport',
+  params: data,
+});
